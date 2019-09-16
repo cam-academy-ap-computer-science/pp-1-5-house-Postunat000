d18ac8e95450768416472eb6823cf2ef13d3a5ac
postunat000 <postunat000@CAM-LAB-21.bgsd.k12.wa.us>
2019-09-16 07:55:03 -0700

    Not done

--- src/House.java
+++ src/House.java
@@ -2,7 +2,25 @@
 public class House {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("This is the house that Jack built.");
+		System.out.println("");
+		System.out.println("This is the malt");
+		System.out.println("That lay in the house that Jack built");
+		System.out.println("");
+		RatThree();
+		System.out.println("");
+		System.out.println("This is the cat,");
+		RatThree();
+		System.out.println("");
+		System.out.println("This is the dog,");
+		CatFour();
+		System.out.println("");
+		System.out.println("This is the cow with the crumpled horn,");
+		DogFive();
+		System.out.println("");
+		System.out.println("This is the maiden all forlorn");
+		CowSix();
+		
 
 	}
 
