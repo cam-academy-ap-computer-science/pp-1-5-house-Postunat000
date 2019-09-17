1cec74d7d56ce6cf8902ce4b0128f7025567c936
postunat000 <postunat000@CAM-LAB-21.bgsd.k12.wa.us>
2019-09-17 07:32:38 -0700

    Done woohooo

--- src/House.java
+++ src/House.java
@@ -2,12 +2,9 @@
 public class House {
 
 	public static void main(String[] args) {
-		System.out.println("This is the house that Jack built.");
-		System.out.println("");
-		System.out.println("This is the malt");
-		System.out.println("That lay in the house that Jack built");
-		System.out.println("");
-		RatThree();
+		Open();
+		System.out.println("This is the rat,");
+		MaltJack();
 		System.out.println("");
 		System.out.println("This is the cat,");
 		RatThree();
@@ -20,8 +17,46 @@ public class House {
 		System.out.println("");
 		System.out.println("This is the maiden all forlorn");
 		CowSix();
-		
-
 	}
-
+	
+	public static void Open() {
+		System.out.println("This is the house that Jack built.");
+		System.out.println("");
+		System.out.println("This is the malt");
+		System.out.println("That lay in the house that Jack built.");
+		System.out.println("");
+	}
+		
+	public static void RatThree() {
+		System.out.println("That killed the rat,");
+		MaltJack();
+	}
+	
+	public static void CatFour() {
+		System.out.println("That worried cat,");
+		System.out.println("That killed the rat,");
+        MaltJack();
+	}
+	
+	public static void DogFive() {
+		TossedDog();
+	}
+	
+	public static void CowSix() {
+		System.out.println("That milked the cow with the crumpled horn,");
+		TossedDog();
+	}
+	
+	public static void TossedDog() {
+		System.out.println("That tossed the dog,");
+		System.out.println("That worried the cat,");
+		System.out.println("That killed the rat,");
+		System.out.println("That ate the malt");
+		System.out.println("That lay in the house that Jack built.");
+	}
+	
+	public static void MaltJack() {
+		System.out.println("That ate the malt");
+		System.out.println("That lay in the house that Jack built.");
+	}
 }
